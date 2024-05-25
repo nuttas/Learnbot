37b248f1aaab787caaffad2fb45ce5cce4e3d9c7
nuttas <11nuttasit@gmail.com>
2024-05-25 10:34:11 +0700

    add some new code 2

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-
+from flask_cors import CORS
 from chat import get_response
 
 app = Flask(__name__)
@@ -7,14 +7,15 @@ app = Flask(__name__)
 
 @app.get("/")
 def index_get():
-    return render_template("base.html")
+    return render_template("base.html", request=request)
+
 
 @app.post("/predict")
 def predict():
-    text = request.get_json().get("massage")
+    text = request.get_json().get("message")
     response = get_response(text)
-    massage = {"answer" : response}
-    return jsonify(massage)
+    message = {"answer" : response}
+    return jsonify(message)
 
 
 if __name__ == "__main__":
